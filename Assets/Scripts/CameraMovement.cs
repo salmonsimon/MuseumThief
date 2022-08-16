7b15f8bc25afe8b8e6aee221b25096c66751ab4a
Simon <simon.bahamonde@gmail.com>
2022-08-16 19:22:24 -0400

    adding basic scripts, animations, tileset and assets

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -4,15 +4,47 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    //private Transform lookAt;
+    [SerializeField] private Transform lookAt;
+    [SerializeField] private float boundX = 0.3f;
+    [SerializeField] private float boundY = 0.15f;
+
+    private void Start()
     {
-        
+        //lookAt = GameManager.instance.player.transform;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void LateUpdate()
     {
-        
+        Vector3 delta = Vector3.zero;
+
+        float deltaX = lookAt.position.x - transform.position.x;
+        float deltaY = lookAt.position.y - transform.position.y;
+
+        if (deltaX < -boundX || deltaX > boundX)
+        {
+            if (transform.position.x < lookAt.position.x)
+            {
+                delta.x = deltaX - boundX;
+            }
+            else
+            {
+                delta.x = deltaX + boundX;
+            }
+        }
+
+        if (deltaY < -boundY || deltaY > boundY)
+        {
+            if (transform.position.y < lookAt.position.y)
+            {
+                delta.y = deltaY - boundY;
+            }
+            else
+            {
+                delta.y = deltaY + boundY;
+            }
+        }
+
+        transform.position += new Vector3(delta.x, delta.y, 0);
     }
 }
