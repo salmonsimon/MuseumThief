43632b80829f034713e77efd017d571e0d6a2636
Simon <simon.bahamonde@gmail.com>
2022-08-19 22:33:47 -0400

    destroying masterpieces instead of disabling them for better performance

--- Assets/Scripts/Interactable/Masterpiece.cs
+++ Assets/Scripts/Interactable/Masterpiece.cs
@@ -17,9 +17,10 @@ public class Masterpiece : Collectable
             collected = true;
             GameManager.instance.GetStolenManager().AddToCarrying(stealable);
 
-            
-            GetComponent<SpriteRenderer>().sprite = empty;
-            GetComponent<BoxCollider2D>().enabled = false;
+
+            //GetComponent<SpriteRenderer>().sprite = empty;
+            //GetComponent<BoxCollider2D>().enabled = false;
+            Destroy(gameObject);
 
         }
     }
