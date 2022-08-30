d30aa5858e73fc1c6694e5c11862100b85c2eecc
salmonsimon <38538239+salmonsimon@users.noreply.github.com>
2022-08-29 23:57:23 -0400

    Added saw item and cell prefab (#20)
    * Added saw item and cell prefab
    
    * Update README.md

---
+++ Assets/Scripts/Interactable/IronCell.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IronCell : Collidable
+{
+    protected override void OnCollide(Collider2D coll)
+    {
+        if (coll.CompareTag("Player"))
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                if (GameManager.instance.GetStolenManager().saw)
+                    Destroy(gameObject);
+                else
+                    GameManager.instance.ShowText("Can't cut it without a saw", 24, Color.white, new Vector3(GameManager.instance.GetPlayer().transform.position.x, GameManager.instance.GetPlayer().transform.position.y + 0.16f, 0), Vector3.up * 40, 1f);
+            }
+
+        }
+    }
+}
