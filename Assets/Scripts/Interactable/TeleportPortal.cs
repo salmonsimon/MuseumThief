cff3d047eedbebee4dbba893d16687e265d97a00
Simon <simon.bahamonde@gmail.com>
2022-09-02 17:14:22 -0400

    Added background music

--- Assets/Scripts/Interactable/TeleportPortal.cs
+++ Assets/Scripts/Interactable/TeleportPortal.cs
@@ -5,12 +5,28 @@ using UnityEngine;
 public class TeleportPortal : Collidable
 {
     [SerializeField] private GameObject teleportPoint;
+    [SerializeField] private AudioClip newSong;
+    private AudioSource audioSource;
+
+    protected override void Start()
+    {
+        base.Start();
+
+        audioSource = GameObject.FindWithTag("Audio").GetComponent<AudioSource>();
+    }
 
     protected override void OnCollide(Collider2D coll)
     {
         if (coll.CompareTag("Player"))
         {
             GameManager.instance.GetPlayer().transform.position = teleportPoint.transform.position;
+
+            if (newSong)
+            {
+                audioSource.clip = newSong;
+                audioSource.Play();
+            }
+
         }
     }
 }
